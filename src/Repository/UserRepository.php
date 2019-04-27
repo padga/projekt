4b4ba6654c7386c2b5a41d23632c755f34edb41b
Patrycja Nowak <patty81420@gmail.com>
2019-04-27 19:28:34 +0200

    users index worx

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * User Repository.
+ * User repository.
  */
-
 namespace App\Repository;
 
 use App\Entity\User;
+use Doctrine\ORM\ORMException;
 use Doctrine\ORM\QueryBuilder;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Bridge\Doctrine\RegistryInterface;
@@ -15,12 +15,11 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  * @method User|null findOneBy(array $criteria, array $orderBy = null)
  * @method User[]    findAll()
  * @method User[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
- * @method           getOrCreateQueryBuilder()
  */
 class UserRepository extends ServiceEntityRepository
 {
     /**
-     * UserRepository constructor.
+     * DefaultCategoryRepository constructor.
      *
      * @param RegistryInterface $registry
      */
@@ -37,19 +36,46 @@ class UserRepository extends ServiceEntityRepository
     public function queryAll(): QueryBuilder
     {
         return $this->getOrCreateQueryBuilder();
+//            ->orderBy('t.email', 'DESC');
     }
 
     /**
      * Save record.
      *
-     * @param \App\Entity\User $user User entity
+     * @param User $user
      *
-     * @throws \Doctrine\ORM\ORMException
+     * @throws ORMException
      * @throws \Doctrine\ORM\OptimisticLockException
      */
     public function save(User $user): void
     {
         $this->_em->persist($user);
         $this->_em->flush($user);
+    }
+
+    /**
+     * Delete record.
+     *
+     * @param User $user
+     *
+     * @throws ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function delete(User $user): void
+    {
+        $this->_em->remove($user);
+        $this->_em->flush($user);
+    }
+
+    /**
+     * Get or create new query builder.
+     *
+     * @param \Doctrine\ORM\QueryBuilder|null $queryBuilder Query builder
+     *
+     * @return \Doctrine\ORM\QueryBuilder Query builder
+     */
+    private function getOrCreateQueryBuilder(QueryBuilder $queryBuilder = null): QueryBuilder
+    {
+        return $queryBuilder ?: $this->createQueryBuilder('t');
     }
 }
